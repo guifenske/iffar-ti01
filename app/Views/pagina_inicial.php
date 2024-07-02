149540e9da434ea871012108fb487e6667b049a8
3K <guifenske@gmail.com>
2024-07-02 10:15:32 -0300

    jash

--- app/Views/pagina_inicial.php
+++ app/Views/pagina_inicial.php
@@ -7,11 +7,11 @@
                     <div class="row gx-5 align-items-center justify-content-center">
                         <div class="col-lg-8 col-xl-7 col-xxl-6">
                             <div class="my-5 text-center text-xl-start">
-                                <h1 class="display-5 fw-bolder text-white mb-2">A Bootstrap 5 template for modern businesses</h1>
-                                <p class="lead fw-normal text-white-50 mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit!</p>
+                                <h1 class="display-5 fw-bolder text-white mb-2">The Fallen Oaks: Natureza Em Ruínas.</h1>
+                                <p class="lead fw-normal text-white-50 mb-4">Aprenda sobre desmatamento enquanto joga!</p>
                                 <div class="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xl-start">
-                                    <a class="btn btn-primary btn-lg px-4 me-sm-3" href="#features">Get Started</a>
-                                    <a class="btn btn-outline-light btn-lg px-4" href="#!">Learn More</a>
+                                    <a class="btn btn-primary btn-lg px-4 me-sm-3" href="#features">Download</a>
+                                    <a class="btn btn-outline-light btn-lg px-4" href="#!">Wiki</a>
                                 </div>
                             </div>
                         </div>
@@ -19,6 +19,8 @@
                     </div>
                 </div>
             </header>
+
+
             <!-- Features section-->
             <section class="py-5" id="features">
                 <div class="container px-5 my-5">
@@ -51,6 +53,8 @@
                     </div>
                 </div>
             </section>
+
+
             <!-- Testimonial section-->
             <div class="py-5 bg-light">
                 <div class="container px-5 my-5">
@@ -71,6 +75,8 @@
                     </div>
                 </div>
             </div>
+
+
             <!-- Blog preview section-->
             <section class="py-5">
                 <div class="container px-5 my-5">
@@ -147,6 +153,8 @@
                             </div>
                         </div>
                     </div>
+
+
                     <!-- Call to action-->
                     <aside class="bg-primary bg-gradient rounded-3 p-4 p-sm-5 mt-5">
                         <div class="d-flex align-items-center justify-content-between flex-column flex-xl-row text-center text-xl-start">
