961eb96b5631b5822f0519e7fbfc6eabb255cbd8
3K <guifenske@gmail.com>
2024-07-09 10:12:46 -0300

    a

--- app/Views/pagina_inicial.php
+++ app/Views/pagina_inicial.php
@@ -25,18 +25,18 @@
 <section class="py-5" id="features">
     <div class="container px-5 my-5">
         <div class="row gx-5">
-            <div class="col-lg-4 mb-5 mb-lg-0"><h2 class="fw-bolder mb-0">A better way to start building.</h2></div>
+            <div class="col-lg-4 mb-5 mb-lg-0"><h2 class="fw-bolder mb-0">Desbrave um mundo pós apocalíptico.</h2></div>
             <div class="col-lg-8">
                 <div class="row gx-5 row-cols-1 row-cols-md-2">
                     <div class="col mb-5 h-100">
                         <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-collection"></i></div>
-                        <h2 class="h5">Featured title</h2>
-                        <p class="mb-0">Paragraph of text beneath the heading to explain the heading. Here is just a bit more text.</p>
+                        <h2 class="h5">Engine Atual</h2>
+                        <p class="mb-0">Bem pensado e em desenvolvimento na Godot Engine.</p>
                     </div>
                     <div class="col mb-5 h-100">
                         <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-building"></i></div>
-                        <h2 class="h5">Featured title</h2>
-                        <p class="mb-0">Paragraph of text beneath the heading to explain the heading. Here is just a bit more text.</p>
+                        <h2 class="h5">Exportação para diferentes plataformas</h2>
+                        <p class="mb-0">É possível exportar o jogo para diferentes plataformas, por exemplo: Android, Windows, Linux, IOS.</p>
                     </div>
                     <div class="col mb-5 mb-md-0 h-100">
                         <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-toggles2"></i></div>
